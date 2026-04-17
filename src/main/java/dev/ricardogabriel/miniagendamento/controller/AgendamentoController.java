e35774608ef45a3a43f3671e54463ca9e94503b8
Gabriel <gabrieloliveira280807@gmail.com>
2026-04-17 18:01:10 -0300

    feat: adicionando controller de agendamento

---
+++ src/main/java/dev/ricardogabriel/miniagendamento/controller/AgendamentoController.java
@@ -0,0 +1,45 @@
+package dev.ricardogabriel.miniagendamento.controller;
+
+import dev.ricardogabriel.miniagendamento.dto.AgendamentoCreateRequest;
+import dev.ricardogabriel.miniagendamento.dto.AgendamentoResponse;
+import dev.ricardogabriel.miniagendamento.dto.AgendamentoUpdateRequest;
+import dev.ricardogabriel.miniagendamento.service.AgendamentoService;
+import jakarta.validation.Valid;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.*;
+
+public class AgendamentoController {
+    private final AgendamentoService service;
+
+    public AgendamentoController(AgendamentoService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public AgendamentoResponse criar(@Valid @RequestBody AgendamentoCreateRequest request) {
+        return service.criar(request);
+    }
+
+    @PutMapping("/{id}")
+    public AgendamentoResponse atualizar(@PathVariable Long id, @Valid @RequestBody AgendamentoUpdateRequest request) {
+        return service.atualizar(id ,request);
+    }
+
+    @PutMapping("/{id}/cancelar")
+    public AgendamentoResponse cancelar(@PathVariable Long id) {
+        return service.cancelar(id);
+    }
+
+    @PutMapping("/{id}/concluir")
+    public AgendamentoResponse concluir(@PathVariable Long id) {
+        return service.concluir(id);
+    }
+
+    @GetMapping("/{id}")
+    public AgendamentoResponse buscarPorId(@PathVariable Long id) {
+        return service.buscarPorId(id);
+    }
+
+
+
+}
